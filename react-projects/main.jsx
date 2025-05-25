99ee884c459f990f97ad48b3eb5d8c58e3da80f6
kapil.raghuwanshi <kapil.raghuwanshi5@gmail.com>
2025-05-25 17:40:39 +0530

    Added react runner with vite setup

---
+++ react-projects/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './progress-bars/App'
+// import App from './file-explorer/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
